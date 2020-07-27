216b48c508a09b49a8229f490e147e5e028ed79d
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-27 17:17:49 -0500

    Added GameObject abstract class.

---
+++ src/gamestate/GameObject.java
@@ -0,0 +1,27 @@
+package gamestate;
+
+/**
+ * A top level abstraction for all objects tracked by the gamestate.
+ */
+public abstract class GameObject {
+    //all data fields for this GameObject. Primitives are stored as their wrapper classes.
+    protected final Object[] fields;
+
+    //when a GameObject is constructed, it must initialize its fields
+    GameObject() {
+        fields = declareFields();
+    }
+
+    //each subclass must implement this method to create an array of the proper size to support its fields
+    protected abstract Object[] declareFields();
+    
+    //access a field by index
+    public Object get(int fieldIndex) {
+        return fields[fieldIndex];
+    }
+
+    //mutate a field by index
+    public void set(int fieldIndex, Object object) {
+        fields[fieldIndex] = object;
+    }
+}
