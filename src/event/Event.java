e227c7d5fa9851944782b11a18a3fb9ec4fd3f6e
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-29 17:50:48 -0500

    Updated to Logger, including automatic log startup and shutdown, entry priorities, and fatal crashes writing to active live logs if open.

---
+++ src/event/Event.java
@@ -0,0 +1,31 @@
+package event;
+
+/**
+ * An event which changes the gamestate.
+ */
+//todo - this may need a complete rework!
+public abstract class Event implements Comparable<Event> {
+    public enum ExecutionOrder {
+        IMMEDIATE,
+        PRIMARY,
+        SECONDARY,
+        FINAL
+    }
+
+    public final ExecutionOrder XO;
+
+    public Event(ExecutionOrder xo) {
+        XO = xo;
+    }
+
+    /**
+     * Sort events by ExecutionOrder.
+     * Events of the same ExecutionOrder need not be sorted further, as their execution in any order must result in
+     * the same final gamestate.
+     */
+    @Override
+    public int compareTo(Event e) {
+        return XO.compareTo(e.XO);
+    }
+
+}
